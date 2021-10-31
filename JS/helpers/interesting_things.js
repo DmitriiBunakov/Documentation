e570219917467f2926be6904907db1d5c314e16f
unknown <mr.bunakov1990@mail.ru>
2021-10-31 23:53:49 +0300

    fsdf

--- JS/helpers/interesting_things.js
+++ JS/helpers/interesting_things.js
@@ -207,3 +207,61 @@
     users.sort(sortByField('Johnson'));
     console.log(users);
 }
+
+
+
+
+
+//? Subscriber
+{
+    class Subscriber {
+        _subscribers = {};
+
+        constructor(initValue) {
+            this._value = initValue;
+        }
+
+        emit(value) {
+            this._value = value;
+            for(const key of Object.getOwnPropertySymbols(this._subscribers)) {
+                this._subscribers[key](this._value);
+            }
+        }
+
+        subscribe(callback) {
+            const symbol = Symbol();
+            this._subscribers[symbol] = callback;
+            this._subscribers[symbol](this._value);
+
+            const _this = this;
+            let subscribeObject = {
+                unsubscribe() {
+                    _this.#unsubscribe(symbol);
+                    subscribeObject = null;
+                },
+            };
+            return subscribeObject;
+        }
+
+        #unsubscribe(functionId) {
+            delete this._subscribers[functionId];
+        }
+    }
+
+    let example = new Subscriber(1);
+
+    let first$ = example.subscribe((value) => {
+            console.log('first', value);
+        });
+
+    example.subscribe((value) => {
+            console.log('second', value);
+        });
+
+    example.emit(2);
+    first$.unsubscribe();
+    example.emit(3);
+    example.emit(4);
+    example.emit(5);
+    example.emit(6);
+}
--- JS/test/script3.js
+++ JS/test/script3.js
@@ -1,16 +1,52 @@
-debugger
-function a() {
-    var name = 'John Doe';
+class Subscriber {
+    _subscribers = {};
 
-    function b() {
-        return name;
+    constructor(initValue) {
+        this._value = initValue;
     }
-    return b;
-}
-var c = a();
 
-function bla() {
-    console.log(1);
+    emit(value) {
+        this._value = value;
+        for(const key of Object.getOwnPropertySymbols(this._subscribers)) {
+            this._subscribers[key](this._value);
+        }
+    }
+
+    subscribe(callback) {
+        const symbol = Symbol();
+        this._subscribers[symbol] = callback;
+        this._subscribers[symbol](this._value);
+
+        const _this = this;
+        let subscribeObject = {
+            unsubscribe() {
+                _this.#unsubscribe(symbol);
+                subscribeObject = null;
+            },
+        };
+        return subscribeObject;
+    }
+
+    #unsubscribe(functionId) {
+        delete this._subscribers[functionId];
+    }
 }
-bla();
-console.log(c());
+
+let example = new Subscriber(1);
+
+let first$ = example.subscribe((value) => {
+        console.log('first', value);
+    });
+
+example.subscribe((value) => {
+        console.log('second', value);
+    }).unsubscribe();
+
+example.emit(2);
+first$.unsubscribe();
+example.emit(3);
+example.emit(4);
+example.emit(5);
+example.emit(6);
+
+
