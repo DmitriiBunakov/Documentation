1c19d12c625f24df2d31eabbf6b863b1e40a1d93
unknown <mr.bunakov1990@mail.ru>
2022-01-25 23:20:13 +0300

    ffdsf

--- TypeScript/test/src/script/index.ts
+++ TypeScript/test/src/script/index.ts
@@ -1,5 +1,19 @@
-class Base {
-    static {
-        console.log(1);
-    }
+function first() {
+    console.log('first(): factory evaluated');
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('first(): called');
+    };
+}
+
+function second() {
+    console.log('second(): factory evaluated');
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('second(): called');
+    };
+}
+
+class ExampleClass {
+    @first()
+    @second()
+    method() {}
 }
---
+++ TypeScript/test/src/script/script1.ts
@@ -0,0 +1,13 @@
+export type TTest = {
+    test: number
+}
+
+export interface ITest {
+    name: string
+}
+
+export function log() {
+    console.log(1);
+}
+
+const value = 1;
