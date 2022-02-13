a6af772c9a4fde591d915806f263bef207555290
DmitryIgorevich <mr.bunakov1990@mail.ru>
2022-02-13 22:16:04 +0300

    a

--- TypeScript/test/src/script/index.ts
+++ TypeScript/test/src/script/index.ts
@@ -1,4 +1,26 @@
-const createKeyboard = (modelID: number) => {
-    const defaultModelID = 23;
-    return {type: 'keyboard', modelID};
-};
+setTimeout(() => {
+    console.log('timer 0');
+});
+
+new Promise(resolve => {
+    console.log('create promise');
+
+    setTimeout(() => {
+        console.log('timer 0 from promise');
+        resolve('resolve');
+    });
+})
+    .then(data => {
+        console.log(data);
+        return 'второй then';
+    })
+    .then(data => {
+        console.log(data);
+    });
+
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
