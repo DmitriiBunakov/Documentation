de7ff7c75a9d5fa8822dce58ec5436e3237f1607
DmitryIgorevich <mr.bunakov1990@mail.ru>
2022-02-17 00:30:36 +0300

    a

--- TypeScript/test/src/script/index.ts
+++ TypeScript/test/src/script/index.ts
@@ -1,26 +1,20 @@
-setTimeout(() => {
-    console.log('timer 0');
-});
+// @ts-ignore
+function twoSum(nums: any[], target: number): number[] {
+    const map: any = {};
+    const length = nums.length;
 
-new Promise(resolve => {
-    console.log('create promise');
+    if (!length) {
+        return [];
+    };
 
-    setTimeout(() => {
-        console.log('timer 0 from promise');
-        resolve('resolve');
-    });
-})
-    .then(data => {
-        console.log(data);
-        return 'второй then';
-    })
-    .then(data => {
-        console.log(data);
-    });
+    for (let index = 0; index < length; index++) {
+        if (map[target - nums[index]] !== undefined) {
+            return [map[target - nums[index]], index];
+        }
 
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
+        map[nums[index]] = index;
+    }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
