7a1036b7eee807508f81aaa798cf2dafbf0197a3
DmitryIgorevich <mr.bunakov1990@gmail.com>
2023-09-26 01:17:17 +0700

    a

--- JS/test/script1.js
+++ JS/test/script1.js
@@ -1,15 +1,15 @@
-function longTask() {
-    let i = 0;
+// function longTask() {
+//     let i = 0;
 
-    for (let index = 0; index < 1_000_000_000_0; index++) {
-        i++;
-    }
+//     for (let index = 0; index < 1_000_000_000_0; index++) {
+//         i++;
+//     }
 
-    console.log(i);
-}
+//     console.log(i);
+// }
 
-longTask();
+// longTask();
 
-document.addEventListener('click', () => {
-    longTask();
-})
+// document.addEventListener('click', () => {
+//     longTask();
+// })
